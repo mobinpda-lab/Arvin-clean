d180a15be9588e54dd9d1d8137df7b57592ecca8
mobinpda-lab <mobinpda@gmail.com>
2026-08-12 11:26:30 +0330

    Add initial widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:arvin/main.dart';
+
+void main() {
+  testWidgets('Arvin starts', (tester) async {
+    await tester.pumpWidget(const ArvinApp());
+    expect(find.text('آروین'), findsOneWidget);
+  });
+}
