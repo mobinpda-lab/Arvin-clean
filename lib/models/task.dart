38b7ecf44b1bae466af7b5903b99c41e3fd47166
mobinpda-lab <mobinpda@gmail.com>
2026-08-12 11:25:33 +0330

    Add task model

---
+++ lib/models/task.dart
@@ -0,0 +1,15 @@
+class Task {
+  Task({required this.id, required this.title, this.description = '', this.followUpDate, this.tags = const [], this.archived = false, this.trashed = false});
+  final String id;
+  String title;
+  String description;
+  DateTime? followUpDate;
+  List<String> tags;
+  bool archived;
+  bool trashed;
+
+  factory Task.fromDescription({required String id, required String description, String? title}) {
+    final first = description.split(RegExp(r'\r?\n')).map((e) => e.trim()).firstWhere((e) => e.isNotEmpty, orElse: () => '');
+    return Task(id: id, title: title?.trim().isNotEmpty == true ? title!.trim() : first, description: description);
+  }
+}
