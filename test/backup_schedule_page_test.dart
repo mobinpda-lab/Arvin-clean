5a066eb4e75d9a4fb94a2d8c773d1ac8cdc61a33
mobinpda-lab <mobinpda@gmail.com>
2026-08-12 17:58:48 +0330

    fix: use package import for backup schedule test

--- test/backup_schedule_page_test.dart
+++ test/backup_schedule_page_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/backup_schedule_page.dart';
+import 'package:arvin/backup_schedule_page.dart';
 
 void main() {
   testWidgets('backup schedule page shows Persian controls', (tester) async {
