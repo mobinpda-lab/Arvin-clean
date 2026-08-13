3f31017e65beba1242269f2710dd2f7d9ecc14bd
mobinpda-lab <mobinpda@gmail.com>
2026-08-14 01:50:36 +0330

    test: fix Jalali calendar month header assertion

--- test/calendar_page_test.dart
+++ test/calendar_page_test.dart
@@ -31,7 +31,7 @@ void main() {
 
     expect(find.text('تماس با مشتری'), findsOneWidget);
     expect(find.text('جلسه تیم'), findsNothing);
-    expect(find.textContaining('۱۴۰۵/۰۵'), findsOneWidget);
+    expect(find.text('۱۴۰۵/۰۵'), findsOneWidget);
     expect(find.textContaining('ساعت ۱۰:۳۰'), findsOneWidget);
     expect(find.textContaining('۱۴۰۵/۰۵/۲۲'), findsOneWidget);
   });
