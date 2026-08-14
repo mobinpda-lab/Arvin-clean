f002362e1e758a6192bb1e9e4e2ef149bb8d2a65
mobinpda-lab <mobinpda@gmail.com>
2026-08-14 17:44:27 +0330

    feat(followup): add durable follow-up model

--- lib/models/follow_up.dart
+++ lib/models/follow_up.dart
@@ -1,1 +1,35 @@
-export 'task.dart' show FollowUp;
+class FollowUp {
+  const FollowUp({
+    required this.id,
+    required this.dateTime,
+    this.note = '',
+    this.result,
+    this.nextFollowUp,
+  });
+
+  final String id;
+  final DateTime dateTime;
+  final String note;
+  final String? result;
+  final DateTime? nextFollowUp;
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'dateTime': dateTime.toIso8601String(),
+        'note': note,
+        'result': result,
+        'nextFollowUp': nextFollowUp?.toIso8601String(),
+      };
+
+  factory FollowUp.fromJson(Map<String, dynamic> json) {
+    return FollowUp(
+      id: json['id'] as String? ?? '',
+      dateTime: DateTime.parse(json['dateTime'] as String),
+      note: json['note'] as String? ?? '',
+      result: json['result'] as String?,
+      nextFollowUp: json['nextFollowUp'] == null
+          ? null
+          : DateTime.tryParse(json['nextFollowUp'] as String),
+    );
+  }
+}
