6032bd12433c7fa7189c8485c60feda46bb5e1cd
mobinpda-lab <mobinpda@gmail.com>
2026-08-14 17:24:23 +0330

    test(followup): mock shared preferences in office widget test

--- test/follow_up_office_page_test.dart
+++ test/follow_up_office_page_test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:arvin/follow_up_office_page.dart';
 
 void main() {
   testWidgets('shows dedicated follow-up office shell', (tester) async {
+    SharedPreferences.setMockInitialValues(<String, Object>{});
     await tester.pumpWidget(const MaterialApp(home: FollowUpOfficePage()));
     await tester.pumpAndSettle();
     expect(find.text('دفتر پیگیری'), findsOneWidget);
