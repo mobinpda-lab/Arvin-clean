039c8a74352c84b342688c2caf5cce94692c5c4f
mobinpda-lab <mobinpda@gmail.com>
2026-08-26 16:27:03 +0330

    fix(followup): add non-platform scheduler adapter

--- lib/automatic_follow_up_scheduler_adapter.dart
+++ lib/automatic_follow_up_scheduler_adapter.dart
@@ -2,3 +2,14 @@ abstract interface class AutomaticFollowUpSchedulerAdapter {
   Future<void> reschedule();
   Future<void> cancel();
 }
+
+class NoopAutomaticFollowUpSchedulerAdapter
+    implements AutomaticFollowUpSchedulerAdapter {
+  const NoopAutomaticFollowUpSchedulerAdapter();
+
+  @override
+  Future<void> reschedule() async {}
+
+  @override
+  Future<void> cancel() async {}
+}
