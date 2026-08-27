554189ee6386ee487834bf13fe3f08b201450a72
mobinpda-lab <mobinpda@gmail.com>
2026-08-27 21:37:24 +0330

    test(android): import Material ValueKey for Home smoke

--- integration_test/android_home_smoke_test.dart
+++ integration_test/android_home_smoke_test.dart
@@ -1,4 +1,5 @@
 import 'package:arvin/main.dart' as app;
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
