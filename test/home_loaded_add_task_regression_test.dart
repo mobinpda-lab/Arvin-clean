513d3c23dc5a60b52a8380a9e08aa79c5e04dabf
mobinpda-lab <mobinpda@gmail.com>
2026-09-08 10:58:01 +0330

    fix(test): restore material types for loaded-task regression

--- test/home_loaded_add_task_regression_test.dart
+++ test/home_loaded_add_task_regression_test.dart
@@ -1,4 +1,5 @@
 import 'package:arvin/main.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
