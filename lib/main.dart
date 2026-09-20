dbe440fba1055dfda7124fe4dbff65fb8740f80c
mobinpda-lab <mobinpda@gmail.com>
2026-09-20 06:48:03 +0330

    fix(home): remove obsolete unused sort helpers

--- lib/main.dart
+++ lib/main.dart
@@ -598,13 +598,6 @@ class _HomePageState extends State<HomePage> {
     return 'کاری برای نمایش وجود ندارد';
   }
 
-  String _sortLabel(TaskListSort sort) => switch (sort) {
-    TaskListSort.date => 'تاریخ کار',
-    TaskListSort.latest => 'آخرین تغییر',
-    TaskListSort.lastFollowUp => 'آخرین پیگیری',
-    TaskListSort.title => 'عنوان',
-  };
-
   String _date(DateTime date) => persianDateFormatter.format(
     date,
     usePersianDate: true,
@@ -1011,14 +1004,6 @@ class _HomePageState extends State<HomePage> {
       selected.clear();
       selectionMode = false;
     });
-  }
-
-  void _setListSort(TaskListSort sort) {
-    setState(() => _listSort = sort);
-  }
-
-  void _toggleSortDirection() {
-    setState(() => _sortDescending = !_sortDescending);
   }
 
   Future<bool> _confirmDeleteForever(Task task) async {
