07b14b15bd3e4ddaa5e83ca8f82b806ea6691a72
mobinpda-lab <mobinpda@gmail.com>
2026-09-24 13:09:19 +0330

    test: reset canonical SQL store before each Flutter test

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -2,7 +2,12 @@ import 'dart:async';
 
 import 'package:flutter_test/flutter_test.dart';
 
+import '../lib/services/task_store.dart';
+
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  setUp(() async {
+    await TaskStore.resetTestDatabase();
+  });
   await testMain();
 }
