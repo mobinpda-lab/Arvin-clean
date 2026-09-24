d52dfb10a8ff546dddd3e2e495cbba66920fe7ac
mobinpda-lab <mobinpda@gmail.com>
2026-09-24 22:03:03 +0330

    test(sql): isolate ProjectStore SQL test database

--- test/project_store_test.dart
+++ test/project_store_test.dart
@@ -4,7 +4,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  setUp(() {
+  setUp(() async {
+    await ProjectStore.resetTestDatabase();
     SharedPreferences.setMockInitialValues(<String, Object>{});
   });
 
