65745ef5cbecffba129d5730a28fb07a32d50e72
mobinpda-lab <mobinpda@gmail.com>
2026-09-24 13:09:28 +0330

    test: use package import for canonical SQL reset

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter_test/flutter_test.dart';
 
-import '../lib/services/task_store.dart';
+import 'package:arvin/services/task_store.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async {
   TestWidgetsFlutterBinding.ensureInitialized();
