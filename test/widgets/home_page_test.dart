9b742c66874bce3f6b3c90a8ddea71f0bddf1679
mobinpda-lab <mobinpda@gmail.com>
2026-09-25 21:56:28 +0330

    test(g2): update Home trash acceptance for canonical action

--- test/widgets/home_page_test.dart
+++ test/widgets/home_page_test.dart
@@ -22,7 +22,7 @@ void main() {
     expect(find.text('پروژه‌ها'), findsOneWidget);
     expect(find.text('دسته‌ها'), findsOneWidget);
     expect(find.text('برچسب‌ها'), findsOneWidget);
-    expect(find.text('کارهای من'), findsNothing);
+    expect(find.text('کارهای من'), findsOneWidget);
   });
 
   testWidgets('loads an existing legacy task from arvin.tasks', (tester) async {
