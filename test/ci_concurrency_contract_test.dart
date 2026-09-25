d61ba341223f8506fc7a4b8684d3428f7330fbba
mobinpda-lab <mobinpda@gmail.com>
2026-09-26 01:01:54 +0330

    CI: update Fast Lane parallel job contract

--- test/ci_concurrency_contract_test.dart
+++ test/ci_concurrency_contract_test.dart
@@ -33,7 +33,7 @@ void main() {
     const draftOnly =
         "github.event_name != 'pull_request' || github.event.pull_request.draft == true";
 
-    expect(parallel.split(draftOnly).length - 1, 2);
+    expect(parallel.split(draftOnly).length - 1, 3);
     expect(parallel, contains("branches: ['wave/**', 'ci/**']"));
     expect(parallel, contains('cancel-in-progress: true'));
     expect(parallel, contains('max-parallel: 6'));
